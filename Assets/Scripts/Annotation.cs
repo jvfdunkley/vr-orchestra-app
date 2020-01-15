de5c93c187680a48439722d08f560e39ca26790c
Jocelyn Dunkley <jocelyn.dunkley09@gmail.com>
2020-01-15 14:43:16 +0100

    initial tutorial implemented
    I implemented the initial tutorial for the parts of the violin scene. It is like a dialogue system that goes through an array of Annotation objects and plays the narration audio, transcript and animation to the specific part.

---
+++ Assets/Scripts/Annotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Annotation
+{
+    public string part_tag;
+    public AudioClip narration;
+    public AnimationClip animation;
+    [TextArea(3,10)]
+    public string narration_transcript;
+}
---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public GameObject continueButton;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI textDisplay;
+    public AudioSource narration_source;
+    public Animator pan_animation;
+	public Annotation[] annotations;
+    public GameObject tag_Panel;
+    public GameObject tutorial_Panel;
+	private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+		StopAllCoroutines();
+        narration_source.clip = annotations[index].narration;
+		StartCoroutine(TypeTranscript());
+	}
+    void Update()
+    {
+        if(textDisplay.text == annotations[index].narration_transcript)
+        {
+            continueButton.SetActive(true);
+        }
+    }
+    IEnumerator TypeTranscript()
+	{
+        if(index == annotations.Length-1)
+        {
+            continueButton.GetComponentInChildren<TextMeshProUGUI>().text = "End tutorial";
+        }
+          
+        pan_animation.SetBool(annotations[index].animation.name, true);
+        titleText.text = annotations[index].part_tag;
+        narration_source.Play();
+        textDisplay.SetText("");
+		foreach (char letter in annotations[index].narration_transcript.ToCharArray())
+		{
+			textDisplay.text += letter;
+            yield return new WaitForSeconds(0.04f);
+		}
+
+	}
+
+    public void DisplayNextSentence()
+    {
+
+        if (index < annotations.Length - 1)
+        {
+            //continueButton.SetActive(false);
+            index++;
+            textDisplay.text = "";
+            narration_source.clip = annotations[index].narration;
+            StopAllCoroutines();
+            StartCoroutine(TypeTranscript());
+
+        }
+        else
+        {
+            //textDisplay.text = "";
+            EndAnnotation();
+            return;
+        }
+    }
+
+
+    void EndAnnotation()
+    {
+        tutorial_Panel.SetActive(false);
+        tag_Panel.SetActive(true);
+        Debug.Log("End of annotations");
+    }
+
+}
